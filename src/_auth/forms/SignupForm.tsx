1449a02fdc35f0e8f9eee8fa112426dcc426d393
M. Maaz <maaz.shakeel.bodla@gmail.com>
2023-12-09 10:23:51 +0700

    feat: call createUserAccount to create a user

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -16,6 +16,8 @@ import { z } from "zod";
 import Loader from "@/components/shared/Loader";
 import { Link } from "react-router-dom";
 
+import { createUserAccount } from "@/lib/appwrite/api";
+
 const SignupForm = () => {
   const isLoading = false;
 
@@ -31,10 +33,9 @@ const SignupForm = () => {
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof SignupValidation>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+  async function onSubmit(values: z.infer<typeof SignupValidation>) {
+    const newUser = await createUserAccount(values);
+    console.log(newUser);
   }
   return (
     <Form {...form}>
