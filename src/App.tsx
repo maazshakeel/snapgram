4dce8b8d90a2975a7d8815df47af51a30d006ee9
M. Maaz <maaz.shakeel.bodla@gmail.com>
2023-12-09 21:12:10 +0700

    feat: add all nessaccary links

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
 
 import { Routes, Route } from "react-router-dom";
 
-import { Home } from "./_root/pages";
 import SigninForm from "./_auth/forms/SigninForm";
 import SignupForm from "./_auth/forms/SignupForm";
 import AuthLayout from "./_auth/AuthLayout";
 import RootLayout from "./_root/RootLayout";
 
-import { Toaster } from "@/components/ui/toaster";
+import {
+  Home,
+  Explore,
+  Saved,
+  AllUsers,
+  CreatePost,
+  EditPost,
+  PostDetails,
+  Profile,
+  UpdateProfile,
+} from "./_root/pages";
 
 const App = () => {
   return (
@@ -23,6 +33,14 @@ const App = () => {
         {/* private routes */}
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/all-users" element={<AllUsers />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:id" element={<EditPost />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
+          <Route path="/profile/:id/*" element={<Profile />} />
+          <Route path="/update-profile/:id" element={<UpdateProfile />} />
         </Route>
       </Routes>
       <Toaster />
