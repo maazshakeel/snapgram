6f09d304d02eeb85a87caf648123ad10c8efbc36
M. Maaz <maaz.shakeel.bodla@gmail.com>
2023-12-10 20:45:40 +0700

    feat: fetch recent posts

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import Loader from "@/components/shared/Loader";
+import { useGetRecentPosts } from "@/lib/react-query/queriesAndMutations";
 
 const Home = () => {
-  return <div>Home</div>;
+  const {
+    data: posts,
+    isPending: isPostLoading,
+    isError: isErrorPosts,
+  } = useGetRecentPosts();
+  return (
+    <div className="flex flex-1">
+      <div className="home-container">
+        <div className="home-posts">
+          <h2 className="h3-bold md:h2-bold text-left w-full">Home Feed</h2>
+          {isPostLoading && !posts ? <Loader /> : <ul>TEST</ul>}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
