5356618f5ba5324f8637cab780315e4041f06902
M. Maaz <maaz.shakeel.bodla@gmail.com>
2023-12-10 22:39:34 +0700

    feat: post like and save done

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,5 +1,7 @@
 import Loader from "@/components/shared/Loader";
+import PostCard from "@/components/shared/PostCard";
 import { useGetRecentPosts } from "@/lib/react-query/queriesAndMutations";
+import { Models } from "appwrite";
 
 const Home = () => {
   const {
@@ -12,7 +14,15 @@ const Home = () => {
       <div className="home-container">
         <div className="home-posts">
           <h2 className="h3-bold md:h2-bold text-left w-full">Home Feed</h2>
-          {isPostLoading && !posts ? <Loader /> : <ul>TEST</ul>}
+          {isPostLoading && !posts ? (
+            <Loader />
+          ) : (
+            <ul className="flex flex-col flex-1 gap-9 w-full">
+              {posts?.documents.map((post: Models.Document, index) => (
+                <PostCard key={post.$id} post={post} />
+              ))}
+            </ul>
+          )}
         </div>
       </div>
     </div>
